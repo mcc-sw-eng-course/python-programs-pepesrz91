094b9f0f080d0d42dbdd8ba7fcb3caa575648f7d
Pepe Suarez <pepesrz@Pepes-MacBook-Pro.local>
2019-02-26 11:06:00 -0600

    Fixed pandas dependency issues in project

--- L7/L7.py
+++ L7/L7.py
@@ -1,5 +1,30 @@
 import csv
+import pandas
+import unittest
+
+def set_input_data(name):
+    return pandas.read_csv(name)
 
 with open ('data.csv') as data:
     print(data)
-    new_table = csv.rea
+    new_table = pandas.read_csv('data.csv')
+    print(new_table.head())
+
+
+class Read_Tests(unittest.TestCase):
+
+    def test_read_cvs(self):
+        new_table = pandas.read_csv('data.csv')
+        another_table = pandas.read_csv('data.csv')
+        self.assertEqual(new_table.empty, False)
+        self.assertEqual(new_table.equals(another_table), True)
+
+    def test_set_input_data(self):
+        first_table = set_input_data('data.csv')
+        new_table = pandas.read_csv('data.csv')
+        self.assertEqual(first_table.equals(new_table), True)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
